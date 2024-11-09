6e666edfbe271aaabc5258513f9913377a3a63fd
Spencer Freeman <154106453+spencermfreeman@users.noreply.github.com>
2024-11-09 00:39:31 -0500

    Add files via upload

--- calibration.py
+++ calibration.py
@@ -1,101 +1,86 @@
-import matplotlib.ticker as ticker
-import os
-import collections 
-from astropy.io import fits
+from astropy.stats import mad_std
+from astropy import units as u
+from astropy.nddata import CCDData
+import ccdproc as ccdp
+import ccdproc as ccdp
+from load_frames import *
 import matplotlib.pyplot as plt
 import numpy as np
-from load_frames import *
+from pathlib import Path
 
-import matplotlib.ticker as ticker
-fig, (ax1, ax2) = plt.subplots(1,2)
+def make_subdir(directory):
+    calibrated_data = Path(directory, 'example1-reduced')
+    calibrated_data.mkdir(exist_ok=True)
+    return calibrated_data
 
-def make_sub_dir(directory):
-    new_directory = directory + "/processed_by_pipeline"
-    if(os.path.exists(new_directory) == False):
-        os.makedirs(new_directory)
-    return new_directory
-
-new_directory = make_sub_dir(directory)
-
-def median_flats(flats, biases, total_flats, total_biases, directory, new_directory):
-   
-    make_sub_dir(directory)
+def create_master_frames(directory):
+    #we should not need to define another imagecollection
+    main_path = Path(directory)
+    files = ccdp.ImageFileCollection(main_path)
     
-    if(len(flats) == 0):
-        print("***image load failure, please check path and retry.***")
-    #case in which only one flat is present
-    elif(len(flats) == 1):
-        
-        bright_value = get_bright_value(median_flat)
-        median_bias = np.median(biases)
-        master_flat_pixel_array = (median_flat - median_bias)/bright_value
-        ax1.imshow(master_flat_pixel_array)
-        ax1.set_title("Master Flat: {0}".format(transit_name))
-        return master_flat_pixel_array
+    biases = files.files_filtered(imagetyp='Bias Frame', include_path=True)
+    flats = files.files_filtered(imagetyp='Flat Field', include_path=True)
     
-    elif(len(flats)>1):
-        '''
-        *to build a master flat image we will take median of the flats and then subtract the average of the biases.*
-        we should probably subtract and then take the median...
-        '''
-        median_flat = np.median(flats, axis=0)
-        bright_value = get_bright_value(median_flat)
-        median_bias = np.median(biases)
-        master_flat_pixel_array = (median_flat - median_bias)/bright_value
-        ax1.imshow(master_flat_pixel_array)
-        write_to_directory(master_flat_pixel_array, 'mflat.fits', new_directory)
-        ax1.set_title("Master Flat: {0}".format(transit_name))
-        return master_flat_pixel_array
+    calibrated_data = make_subdir(directory)
     
-def median_biases(biases, total_biases, directory, new_directory):
-    make_sub_dir(directory)
-    if(len(biases) == 0):
-        print("***image load failure, please check path and retry.***")
-    elif(len(biases) == 1):
-        master_bias_pixel_array = biases[0][0].data
-        plt.imshow(master_bias)
-        ax2.set_title("Master Bias: {0}".format(transit_name))
-        ax2.yaxis.set_major_locator(ticker.NullLocator())
-        write_to_directory(master_bias_pixel_array, 'mbias.fits', new_directory)
-        return master_bias_pixel_array
-    elif(len(biases) > 1):
-        master_bias_pixel_array = np.median(biases, axis=0)
-        ax2.imshow(master_bias_pixel_array)
-        ax2.set_title("Master Bias: {0}".format(transit_name))
-        #hide the tick marks on the master bias plot
-        ax2.yaxis.set_major_locator(ticker.NullLocator())
-        write_to_directory(master_bias_pixel_array, 'mbias.fits', new_directory)
-        return master_bias_pixel_array
+    # Load the bias frames as CCDData with appropriate units
+    bias_data = []
+    for bias_file in biases:
+        # Read each file into a CCDData object with units (e.g., "adu" for analog-to-digital units)
+        bias_frame = CCDData.read(bias_file, unit=u.adu)
+        bias_data.append(bias_frame)
+    
+    reduced_images = ccdp.ImageFileCollection(calibrated_data)
+    master_bias = ccdp.combine(bias_data, method='average', sigma_clip=True, sigma_clip_low_thresh=5, sigma_clip_high_thresh=5,
+                                sigma_clip_func=np.ma.median, sigma_clip_dev_func=mad_std, mem_limit=350e6)
 
-def get_bright_value(master_flat_pixel_array_pre_sub):
-    values = []
-    dimension = int(return_dimensions(master_flat_pixel_array_pre_sub)[0])
-    dist = 100
-    for i in range(dimension//2-dist, dimension//2+dist):
-        for j in range(dimension//2-dist, dimension//2+dist):
-            values.append(master_flat_pixel_array_pre_sub[i][j])
-    median_bright = np.median(values)
-    return median_bright
-    
-def fits_to_array(fits_list):
-    pixel_arrays = []
-    for file in fits_list:
-        pixel_arrays.append(file[0].data)
-    return pixel_arrays
+    master_bias.meta['combined'] = True
+    master_bias.write(calibrated_data / 'master_bias.fit', overwrite=True)
 
-def write_to_directory(master_pixel_array, filename, new_directory):
-    path = new_directory
-    hdu = fits.PrimaryHDU(master_pixel_array)
-    hdulist = fits.HDUList([hdu])
-    hdulist.writeto(os.path.join(new_directory, filename), overwrite=True)
+    '''
+    Make the master flat:
+    it is assumed that only r' filter is used in observation, if multiple filters were used to create 
+    multiple flats, one master flat should be created per filter
+    '''
     
-def return_dimensions(pixel_2d_array):
-    return ([len(pixel_2d_array), len(pixel_2d_array[0])])
+    flat_data = []
+    for flat_file in flats:
+        flat_field = CCDData.read(flat_file, unit=u.adu)
+        flat_data.append(flat_field)
+            
+    master_flat = ccdp.combine(flat_data, method='average', scale=inv_median, sigma_clip=True, sigma_clip_low_thresh=5, sigma_clip_high_thresh=5,
+                                sigma_clip_func=np.ma.median, signma_clip_dev_func=mad_std, mem_limit=350e6)
 
+    master_flat.meta['combined'] = True
+    master_flat.write(calibrated_data / 'master_flat.fit', overwrite = True)
+    
+    return master_bias, master_flat
+
+#this is used in the stats associated with the flat field combination process, see arguments of ccdp.combine() function
+def inv_median(a):
+    return 1 / np.median(a)
+    
+def calibrate_light_frames(directory, transit_name, master_bias, master_flat):
+    
+    main_path = Path(directory)
+    files = ccdp.ImageFileCollection(main_path)
+    transit_name = transit_name
+    #obtain all of the light frames
+    lights = files.files_filtered(imagetyp='Light Frame', include_path=True)
+
+    #process each light frame in a compact manner and add to list, might need to add a list of the processed lights, removed to save on space.
+    counter=1
+    for light in lights:
+        light_frame = CCDData.read(light, unit=u.adu)
+        reduced = ccdp.ccd_process(light_frame, master_bias=master_bias, master_flat=master_flat)
+        reduced.write('{0}/example1-reduced/{1}_lrp_out_{2}.fit'.format(directory, transit_name, counter), overwrite=True)
+        counter+=1
+    
 if __name__ == "__main__":
-    flat_pixel_array_list = fits_to_array(flats)
-    bias_pixel_array_list = fits_to_array(biases)
-    lights_pixel_array_list = fits_to_array(lights)
-    master_flat_pixel_array = median_flats(flat_pixel_array_list, bias_pixel_array_list, len(flats), len(biases), directory, new_directory)
-    master_bias_pixel_array = median_biases(bias_pixel_array_list, len(biases), directory, new_directory)
-    plt.show()
+    #writes the master flat and master bias to the subdirectory using newer method
+    #form of path may be important as of now, this should not matter.
+    dir = '/Users/spencerfreeman/Desktop/stepUp/2024-09-4-skuban'
+    transit_name = 'qatar-5b'
+    master_flat, master_bias = create_master_frames(dir)
+    calibrate_light_frames(dir, transit_name, master_flat, master_bias)
+    
