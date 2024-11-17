0e668a915b07549958df8970c830ae07872fef4a
Spencer Freeman <154106453+spencermfreeman@users.noreply.github.com>
2024-11-16 20:48:57 -0500

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 from calibration import create_master_frames, calibrate_light_frames
-from astrometry_connection import * 
 import matplotlib.pyplot as plt
 from astropy.io import fits
+from photometry import perform_photometry
 
 def calibrate(dir, transit_name):
     master_flat, master_bias = create_master_frames(dir)
@@ -9,14 +9,18 @@ def calibrate(dir, transit_name):
     #in case we want to see master composite calibration frames
     return lights_calibrated
 
-def solve():
-    plate_solve_frame_list(paths)
-
+#this will become a measurement of realtive flux
+def plot(time, target_flux, comp_flux, title):
+    fig, axs = plt.subplots()
+    axs.plot(time, target_flux)
+    axs.set_title(title)
+    plt.show()
+    
 if __name__ == "__main__":
     dir = '/Users/spencerfreeman/Desktop/stepUp/2024-09-4-skuban'
     transit_name = 'qatar-5b'
     lights_calibrated = calibrate(dir, transit_name)
-    hdulist = lights_calibrated[0].to_hdu()
-    plt.imshow(hdulist[0].data)
-    plt.show()
+    #lights_calibrated is a list of CCDData objects, arrays are accsessd by data attribute.
+    target_flux, comparison_flux, time = perform_photometry(lights_calibrated)
+    plot(time, target_flux, comparison_flux, transit_name)
     
--- photometry.py
+++ photometry.py
@@ -1,12 +1,9 @@
 import numpy as np
 from astropy.stats import sigma_clipped_stats
-from astropy.io import fits
 import matplotlib.pyplot as plt
 from astropy.nddata import Cutout2D
 from astropy import units as u
 import matplotlib.pyplot as plt
-from astropy.visualization import SqrtStretch
-from astropy.visualization.mpl_normalize import ImageNormalize
 from astropy.stats import SigmaClip
 from photutils.background import Background2D, MedianBackground
 from photutils.detection import DAOStarFinder
@@ -31,19 +28,18 @@ def example_load(path):
     lights = files.files_filtered(imagetyp='Light Frame', include_path=True)
     return lights
 
-def get_flux(light_frame_CCDData, x_pix_est, y_pix_est, target_name):
+def get_flux(light_frame, x_pix_est, y_pix_est, target_name, size_cutout, position_cutout):
     #we must accsess the array with .data property of light_frame
-    light_frame = CCDData.read(light_frame_CCDData, unit=u.adu)
     light_arr = light_frame.data
     
-    position = (2400, 2000)
-    size = (500, 500)# pixels subject to change.
+    position = position_cutout
+    size = size_cutout # pixels subject to change
     cutout = Cutout2D(light_arr, position, size)
     
     sigma_clip = SigmaClip(sigma=3.0)
     bkg_estimator = MedianBackground()
     
-    #we need to be sure cutout is in the actual frame
+    #we need to be sure cutout is in the actual frame, also deal with the (50,50) tuple.
     bkg = Background2D(cutout.data, (50, 50), filter_size=(3, 3), sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
     cutout.data = cutout.data-bkg.background #subtract background from cutout data
     sources = get_sources(cutout.data)
@@ -53,7 +49,7 @@ def get_flux(light_frame_CCDData, x_pix_est, y_pix_est, target_name):
     #get the radial profile, this array might need to be larger
     edge_radii = np.arange(25)
     rp = RadialProfile(cutout.data, xycen, edge_radii, mask=None)
-    #plot_and_save(rp, "Seeing Profile", target_name)
+    plot_and_save(rp, "Seeing Profile", target_name, "sp")
     print(x_objective)
     print(y_objective)
     sources.pprint()
@@ -66,13 +62,13 @@ def return_flux_array(cutout_data, xycen, target_name):
     radii = np.arange(1, 26)
     cog = CurveOfGrowth(cutout_data, xycen, radii, mask=None)
     flux_arr = cog.profile
-    plot_and_save(cog, "Curve of Growth", target_name)
+    plot_and_save(cog, "Curve of Growth", target_name, "cog")
     return flux_arr
     
-def plot_and_save(obj, title, star_label):
+def plot_and_save(obj, title, star_label, diff):
     obj.plot(label='Radial Profile')
     plt.title("{0} {1}".format(title, star_label))
-    plt.savefig('/Users/spencerfreeman/Desktop/stepUp/2024-09-4-skuban/example1-reduced/test')
+    plt.savefig('/Users/spencerfreeman/Desktop/stepUp/2024-09-4-skuban/example1-reduced/{0}'.format(diff))
 
 def get_sources(cutout_data):
     mean, median, std = sigma_clipped_stats(cutout_data, sigma=3.0)  
@@ -111,6 +107,7 @@ def get_pixel_location(x_centoroids, y_centroids, x_pix_est, y_pix_est):
 #we will be passing a CCDData object
 def perform_photometry(light_frames):
     
+    #TODO: these values must not cause errors if the cutout region is changed.
     target_x_est = 252.03    
     target_y_est = 327.26
     
@@ -120,18 +117,24 @@ def perform_photometry(light_frames):
     
     target_flux_values = []
     comparison_flux_values = []
-    
+    time = []
     for i,light_frame_CCDData in enumerate(light_frames):
-        
-        flux_tar = get_flux(light_frame_CCDData, target_x_est, target_y_est, "Target")
+        light_frame = CCDData.read(light_frame_CCDData, unit=u.adu)
+        flux_tar = get_flux(light_frame, target_x_est, target_y_est, "Target", (500, 500), (2400, 2000))
         #TODO get better etsimates for comp star
-        #flux_comp = get_flux(light_frame_CCDData, comp_x_est, comp_y_est, "Comparison")
+        #flux_comp = get_flux(light_frame_CCDData, comp_x_est, comp_y_est, "Comparison", (500, 500))
         
         target_flux_values.append(flux_tar)
         #comparison_flux_values.append(flux_comp)
+        time.append(light_frame.header['DATE-OBS'])
         
-    return target_flux_values, comparison_flux_values
+        
+    return target_flux_values, comparison_flux_values, time
 
 if __name__ == "__main__":
     lights = example_load('/Users/spencerfreeman/Desktop/stepUp/2024-09-4-skuban/example1-reduced/test')
-    perform_photometry(lights)
+    tar_flux_arr, comp_flux_arr, time = perform_photometry(lights)
+    fig, ax = plt.subplots()
+    ax.scatter(time, tar_flux_arr)
+    plt.show()
+    
